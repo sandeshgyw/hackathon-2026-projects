d7d3b625d0e4711187cf1e0a41c166d383f522af
sandeshgyw <sandeshgyanwali@gmail.com>
2026-04-25 11:52:02 -0500

    added care state model

---
+++ projects/MediMeal/src/medimeal/lib/models/care_state.dart
@@ -0,0 +1,13 @@
+class CareState {
+  final String summary;
+  final String caution;
+  final int weeklyUsage;
+  final int weeklyLimit;
+
+  CareState({
+    required this.summary,
+    required this.caution,
+    required this.weeklyUsage,
+    required this.weeklyLimit,
+  });
+}
