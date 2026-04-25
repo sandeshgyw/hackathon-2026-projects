b6663328ce350224059d5b8ce7d84041467cf831
sandeshgyw <sandeshgyanwali@gmail.com>
2026-04-25 11:51:28 -0500

    added medication model

---
+++ projects/MediMeal/src/medimeal/lib/models/medications.dart
@@ -0,0 +1,17 @@
+class Medication {
+  final String id;
+  final String name;
+  final String dosage;
+  final String time;
+  final String instructions;
+  final String workflowType;
+
+  Medication({
+    required this.id,
+    required this.name,
+    required this.dosage,
+    required this.time,
+    required this.instructions,
+    required this.workflowType,
+  });
+}
